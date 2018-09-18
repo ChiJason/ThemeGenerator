b8a4234413935e30523bc0f0e3cef22f47a6a6d9
紀宣志 <jason79720@gmail.com>
2018-09-18 23:39:30 +0800

    Initial commit

---
+++ app/src/main/java/ez/invoice/jason/themegenerator/MainActivity.kt
@@ -0,0 +1,12 @@
+package ez.invoice.jason.themegenerator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
