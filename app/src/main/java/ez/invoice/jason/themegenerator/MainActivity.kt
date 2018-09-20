dcba695c63d9ec17a22caa5ca46affb3c4d10c38
JasonChi <jason79720@gmail.com>
2018-09-20 18:51:16 +0800

    add test case - change color after color hex input

--- app/src/main/java/ez/invoice/jason/themegenerator/MainActivity.kt
+++ app/src/main/java/ez/invoice/jason/themegenerator/MainActivity.kt
@@ -2,6 +2,8 @@ package ez.invoice.jason.themegenerator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +12,17 @@ class MainActivity : AppCompatActivity() {
     private val primaryColorLight by lazy { activity_main_primary_color_light_edt }
     private val primaryColorDark by lazy { activity_main_primary_color_dark_edt }
     private val primaryBlack by lazy { activity_main_primary_black_edt }
+    private val primaryColorText by lazy { activity_main_primary_color }
+    private val primaryColorLightText by lazy { activity_main_primary_color_light }
+    private val primaryColorDarkText by lazy { activity_main_primary_color_dark }
+    private val primaryBlackText by lazy { activity_main_primary_black }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupView()
+        setupListener()
     }
 
     private fun setupView() {
@@ -24,4 +31,9 @@ class MainActivity : AppCompatActivity() {
         primaryColorDark.setText("d7f2fc")
         primaryBlack.setText("37474f")
     }
+
+    private fun setupListener() {
+
+    }
+
 }
--- app/src/test/java/ez/invoice/jason/themegenerator/MainActivityTest.kt
+++ app/src/test/java/ez/invoice/jason/themegenerator/MainActivityTest.kt
@@ -1,8 +1,10 @@
 package ez.invoice.jason.themegenerator
 
+import android.graphics.Color
 import android.view.View
 import android.view.View.VISIBLE
 import android.widget.EditText
+import android.widget.TextView
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
@@ -14,10 +16,26 @@ import org.junit.Before
 class MainActivityTest {
 
     private lateinit var mainActivity: MainActivity
+    private lateinit var primaryText: TextView
+    private lateinit var primaryLightText: TextView
+    private lateinit var primaryDarkText: TextView
+    private lateinit var primaryBlackText: TextView
+    private lateinit var primaryEdt: EditText
+    private lateinit var primaryLightEdt: EditText
+    private lateinit var primaryDarkEdt: EditText
+    private lateinit var primaryBlackEdt: EditText
 
     @Before
     fun setupTest() {
         mainActivity = Robolectric.buildActivity(MainActivity::class.java).create().get()
+        primaryText = mainActivity.findViewById(R.id.activity_main_primary_color)
+        primaryLightText = mainActivity.findViewById(R.id.activity_main_primary_color_light)
+        primaryDarkText = mainActivity.findViewById(R.id.activity_main_primary_color_dark)
+        primaryBlackText = mainActivity.findViewById(R.id.activity_main_primary_black)
+        primaryEdt = mainActivity.findViewById(R.id.activity_main_primary_color_edt)
+        primaryLightEdt = mainActivity.findViewById(R.id.activity_main_primary_color_light_edt)
+        primaryDarkEdt = mainActivity.findViewById(R.id.activity_main_primary_color_dark_edt)
+        primaryBlackEdt = mainActivity.findViewById(R.id.activity_main_primary_black_edt)
     }
 
     @Test
@@ -27,19 +45,39 @@ class MainActivityTest {
     }
 
     @Test
-    fun mainActivityStartWithDefaultThemeValues() {
+    fun start_view_with_default_theme_values() {
 
-        val primaryColor = mainActivity.findViewById<EditText>(R.id.activity_main_primary_color_edt).text.toString().trim()
+        val primaryColor = primaryEdt.text.toString().trim()
         assertEquals("00aeef", primaryColor)
 
-        val primaryColorLight = mainActivity.findViewById<EditText>(R.id.activity_main_primary_color_light_edt).text.toString().trim()
+        val primaryColorLight = primaryLightEdt.text.toString().trim()
         assertEquals("d7f2fc", primaryColorLight)
 
-        val primaryColorDark = mainActivity.findViewById<EditText>(R.id.activity_main_primary_color_dark_edt).text.toString().trim()
+        val primaryColorDark = primaryDarkEdt.text.toString().trim()
         assertEquals("d7f2fc", primaryColorDark)
 
-        val primaryBlack = mainActivity.findViewById<EditText>(R.id.activity_main_primary_black_edt).text.toString().trim()
+        val primaryBlack = primaryBlackEdt.text.toString().trim()
         assertEquals("37474f", primaryBlack)
+    }
+
+    @Test
+    fun textView_color_should_change_when_valid_colorHex_is_input() {
+
+        primaryEdt.setText("b5c7d5")
+        primaryLightEdt.setText("526b7a")
+        primaryDarkEdt.setText("293641")
+        primaryBlackEdt.setText("0d1114")
+
+        val primaryColor = primaryText.currentTextColor
+        val primaryLightColor = primaryLightText.currentTextColor
+        val primaryDarkColor = primaryDarkText.currentTextColor
+        val primaryBlackColor = primaryBlackText.currentTextColor
+
+        assertEquals(Color.parseColor("#b5c7d5"), primaryColor)
+        assertEquals(Color.parseColor("#526b7a"), primaryLightColor)
+        assertEquals(Color.parseColor("#293641"), primaryDarkColor)
+        assertEquals(Color.parseColor("#0d1114"), primaryBlackColor)
+
     }
 
 }
